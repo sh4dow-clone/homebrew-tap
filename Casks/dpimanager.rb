1510dab638efa956f1518ca87e2026035bdcba6b
Harsh <131232263+0xDranzer@users.noreply.github.com>
2026-05-30 22:44:29 +0530

    Update dpimanager.rb

--- Casks/dpimanager.rb
+++ Casks/dpimanager.rb
@@ -1,5 +1,5 @@
 cask "dpimanager" do
-  version "1.0.0"
+  version "1.1.0"
   sha256 "eafd8f5178000595c96a52a938a08cf853022140f651820488cd422bdaa9482f"
 
   url "https://github.com/0xDranzer/MAC_DPIManager/releases/download/v#{version}/DPIManager.zip"
