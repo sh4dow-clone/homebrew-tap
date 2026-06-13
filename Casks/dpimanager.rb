f808c02c9772698dac37bec63be656f61a13f41e
Harsh <131232263+shad0w-cl0nE@users.noreply.github.com>
2026-06-13 11:20:16 +0530

    Update dpimanager.rb

--- Casks/dpimanager.rb
+++ Casks/dpimanager.rb
@@ -2,7 +2,7 @@ cask "dpimanager" do
   version "1.1.0"
   sha256 "2c89cce7bd37b80469c6b0f511597e7e9135318eb6de54ae0495da6c31baa6bc"
 
-  url "https://github.com/shad0w-cl0nE/MAC_DPIManager/releases/download/v#{version}/DPIManager.zip"
+  url "https://github.com/HasBrain/MAC_DPIManager/releases/download/v#{version}/DPIManager.zip"
   name "DPIManager"
   desc "Enable HiDPI modes and adjust font smoothing for external displays"
   homepage "https://github.com/shad0w-cl0nE/MAC_DPIManager"
