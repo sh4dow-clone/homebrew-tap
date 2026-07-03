f6680f5c1133405eb57ce1c9841dc11eb003efac
Harsh <131232263+sh4dow-clone@users.noreply.github.com>
2026-07-03 17:14:41 +0530

    Update faceunlock.rb

--- Casks/faceunlock.rb
+++ Casks/faceunlock.rb
@@ -2,7 +2,7 @@ cask "faceunlock" do
   version "1.0.0"
   sha256 "6b6a90db2e9533ea68060968116a69cd6bc7742ab7df00206b85ad80cff8dd63"
 
-  url "https://github.com/HasBrain/FaceUnlock/releases/download/v#{version}/FaceUnlock-v#{version}.zip"
+  url "https://github.com/HasBrain/FaceUnlock/releases/download/v#{version}/FaceUnlock.zip"
   name "FaceUnlock"
   desc "Face unlock for macOS"
 
