1d1c94bfe37600a44006b0bc24e8f57f6386748b
Harsh <131232263+sh4dow-clone@users.noreply.github.com>
2026-07-18 09:27:35 +0530

    Update faceunlock.rb

--- Casks/faceunlock.rb
+++ Casks/faceunlock.rb
@@ -1,10 +1,15 @@
 cask "faceunlock" do
   version "1.0.0"
-  sha256 "707e9a99f36239b8e3d80abec872943a0f00c3be79b60a6ce61ea1763f0c6826"
+  sha256 "PASTE_NEW_SHA256_HERE"
 
   url "https://github.com/HasBrain/FaceUnlock/releases/download/v#{version}/FaceUnlock.zip"
   name "FaceUnlock"
   desc "Face unlock for macOS"
 
   app "FaceUnlock.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+      args: ["-dr", "com.apple.quarantine", "#{appdir}/FaceUnlock.app"]
+  end
 end
